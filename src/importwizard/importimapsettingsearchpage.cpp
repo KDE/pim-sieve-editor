234a2108a760aebef6ced30fdac8f5e21b6a8a03
Luigi Toscano <luigi.toscano@tiscali.it>
2016-09-08 23:52:42 +0200

    UI strings: fix capitalization

--- src/importwizard/importimapsettingsearchpage.cpp
+++ src/importwizard/importimapsettingsearchpage.cpp
@@ -29,7 +29,7 @@ ImportImapSettingSearchPage::ImportImapSettingSearchPage(QWidget *parent)
     : QWidget(parent)
 {
     QVBoxLayout *mainLayout = new QVBoxLayout(this);
-    QLabel *label = new QLabel(i18n("Please select the program from which you like to import IMAP Settings:"), this);
+    QLabel *label = new QLabel(i18n("Please select the program from which you like to import IMAP settings:"), this);
     label->setObjectName(QStringLiteral("label"));
     mainLayout->addWidget(label);
 
