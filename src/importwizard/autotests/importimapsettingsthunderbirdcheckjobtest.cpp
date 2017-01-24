be97ac72183d883ee252b665b60c847a76757439
Montel Laurent <montel@kde.org>
2017-01-24 13:45:57 +0100

    Start to create autotest for it

---
+++ src/importwizard/autotests/importimapsettingsthunderbirdcheckjobtest.cpp
@@ -0,0 +1,16 @@
+#include "importimapsettingsthunderbirdcheckjobtest.h"
+
+#include <QTest>
+
+ImportImapSettingsThunderbirdCheckJobTest::ImportImapSettingsThunderbirdCheckJobTest(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+ImportImapSettingsThunderbirdCheckJobTest::~ImportImapSettingsThunderbirdCheckJobTest()
+{
+
+}
+
+QTEST_MAIN(ImportImapSettingsThunderbirdCheckJobTest)
---
+++ src/importwizard/autotests/importimapsettingsthunderbirdcheckjobtest.h
@@ -0,0 +1,14 @@
+#ifndef IMPORTIMAPSETTINGSTHUNDERBIRDCHECKJOBTEST_H
+#define IMPORTIMAPSETTINGSTHUNDERBIRDCHECKJOBTEST_H
+
+#include <QObject>
+
+class ImportImapSettingsThunderbirdCheckJobTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ImportImapSettingsThunderbirdCheckJobTest(QObject *parent = nullptr);
+    ~ImportImapSettingsThunderbirdCheckJobTest();
+};
+
+#endif // IMPORTIMAPSETTINGSTHUNDERBIRDCHECKJOBTEST_H
