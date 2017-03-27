e515208064954fbade33e902b85e480ab3d35814
Montel Laurent <montel@kde.org>
2017-03-27 14:47:23 +0200

    Add class for importing password. We can autotest it after that

---
+++ src/importwizard/checks/abstractimapsettingspassword.h
@@ -0,0 +1,34 @@
+/*
+   Copyright (C) 2017 Montel Laurent <montel@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef ABSTRACTIMAPSETTINGSPASSWORD_H
+#define ABSTRACTIMAPSETTINGSPASSWORD_H
+
+#include <sieveeditorutil.h>
+
+#include "libsieveeditor_private_export.h"
+class LIBSIEVEEDITOR_TESTS_EXPORT AbstractImapSettingsPassword
+{
+public:
+    AbstractImapSettingsPassword();
+    virtual ~AbstractImapSettingsPassword();
+    virtual void importPasswords(SieveEditorUtil::SieveServerConfig &config, const KSharedConfigPtr &resourceConfig, bool reuseImapSettings);
+};
+
+#endif // ABSTRACTIMAPSETTINGSPASSWORD_H
---
+++ src/importwizard/checks/importimapsettingsakonadipassword.cpp
@@ -0,0 +1,61 @@
+/*
+   Copyright (C) 2017 Montel Laurent <montel@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "importimapsettingsakonadipassword.h"
+#include "sieveserversettings.h"
+#include <KWallet>
+
+ImportImapSettingsAkonadiPassword::ImportImapSettingsAkonadiPassword()
+    : AbstractImapSettingsPassword()
+{
+
+}
+
+ImportImapSettingsAkonadiPassword::~ImportImapSettingsAkonadiPassword()
+{
+
+}
+
+
+void ImportImapSettingsAkonadiPassword::importPasswords(SieveEditorUtil::SieveServerConfig &config, const KSharedConfigPtr &resourceConfig, bool reuseImapSettings)
+{
+    KWallet::Wallet *wallet = SieveServerSettings::self()->wallet();
+    QString password;
+    QString customPassword;
+    if (wallet) {
+        bool passwordStoredInWallet = false;
+        if (wallet && wallet->hasFolder(QStringLiteral("imap"))) {
+            wallet->setFolder(QStringLiteral("imap"));
+            wallet->readPassword(resourceConfig->name(), password);
+            if (!reuseImapSettings) { //Custom Password
+                wallet->readPassword(QStringLiteral("custom_sieve_") + resourceConfig->name(), customPassword);
+            }
+            passwordStoredInWallet = true;
+        }
+        if (passwordStoredInWallet) {
+            config.sieveImapAccountSettings.setPassword(password);
+            if (reuseImapSettings) {
+                config.sieveSettings.password = password;
+            } else {
+                config.sieveSettings.password = customPassword;
+            }
+        }
+    }
+
+}
