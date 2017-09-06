32c75fdcb44fda600061da833a7d95097c9af2cd
Montel Laurent <montel@kde.org>
2017-09-06 08:54:14 +0200

    Assign parent directly

--- src/sieveeditorconfiguredialog.cpp
+++ src/sieveeditorconfiguredialog.cpp
@@ -36,9 +36,8 @@ SieveEditorConfigureDialog::SieveEditorConfigureDialog(QWidget *parent)
     : QDialog(parent)
 {
     setWindowTitle(i18n("Configure"));
-    QDialogButtonBox *buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
-    QVBoxLayout *mainLayout = new QVBoxLayout;
-    setLayout(mainLayout);
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
+    QDialogButtonBox *buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, this);
     QPushButton *okButton = buttonBox->button(QDialogButtonBox::Ok);
     okButton->setDefault(true);
     okButton->setShortcut(Qt::CTRL | Qt::Key_Return);
