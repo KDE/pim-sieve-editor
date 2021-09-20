c2d87d2ec3ea3f5d819be2bc9eb45c70d48a621c
Laurent Montel <montel@kde.org>
2021-09-20 21:04:55 +0200

    GIT_SILENT: use pragma once here too

--- src/sieveeditor-version.h.cmake
+++ src/sieveeditor-version.h.cmake
@@ -4,10 +4,5 @@
    SPDX-License-Identifier: GPL-2.0-or-later
 */
 
-#ifndef SIEVEEDITOR_VERSION_H
-#define SIEVEEDITOR_VERSION_H
-
-
+#pragma once
 #define SIEVEEDITOR_VERSION "@KDEPIM_VERSION@"
-
-#endif
