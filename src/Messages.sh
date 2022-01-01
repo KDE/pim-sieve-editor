5bc5901072619e57e33e9cb2338d5f98c620c5b1
Laurent Montel <montel@kde.org>
2022-01-01 16:17:54 +0100

    GIT_SILENT: happy new year

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 # SPDX-License-Identifier: CC0-1.0
-# SPDX-FileCopyrightText: 2013-2021 Laurent Montel <montel@kde.org> 
+# SPDX-FileCopyrightText: 2013-2022 Laurent Montel <montel@kde.org> 
 $EXTRACTRC `find . -name '*.ui' -or -name '*.rc'` >> rc.cpp || exit 11
 $XGETTEXT `find . -name '*.cpp' -o -name '*.h' | grep -v '/autotests/'` -o $podir/sieveeditor.pot
 rm -f rc.cpp
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 /*
-   SPDX-FileCopyrightText: 2013-2021 Laurent Montel <montel@kde.org>
+   SPDX-FileCopyrightText: 2013-2022 Laurent Montel <montel@kde.org>
 
    SPDX-License-Identifier: GPL-2.0-or-later
 */
@@ -46,7 +46,7 @@ int main(int argc, char **argv)
                          QStringLiteral(SIEVEEDITOR_VERSION),
                          i18n("Sieve Editor"),
                          KAboutLicense::GPL_V2,
-                         i18n("Copyright © 2013-2021 sieveeditor authors"));
+                         i18n("Copyright © 2013-%1 sieveeditor authors", QStringLiteral("2022")));
     aboutData.addAuthor(i18n("Laurent Montel"), i18n("Maintainer"), QStringLiteral("montel@kde.org"));
 
     QApplication::setWindowIcon(QIcon::fromTheme(QStringLiteral("kmail")));
