8d8ccc9dc81ef7f315124011769c50afe224e447
Laurent Montel <montel@kde.org>
2022-01-30 19:55:04 +0100

    Purpose-qt6 compiles fine now

--- src/sieveeditormainwindow.cpp
+++ src/sieveeditormainwindow.cpp
@@ -185,14 +185,12 @@ void SieveEditorMainWindow::setupActions()
     ac->addAction(QStringLiteral("import_script"), mImportAction);
     mImportAction->setEnabled(false);
 
-#ifdef HAVE_PURPOSE_SUPPORT
     auto purposeMenu = new SievePurposeMenuWidget(this, this);
     mShareAction = new QAction(i18n("Share..."), this);
     ac->addAction(QStringLiteral("share_script"), mShareAction);
     mShareAction->setMenu(purposeMenu->menu());
     mShareAction->setIcon(QIcon::fromTheme(QStringLiteral("document-share")));
     purposeMenu->setEditorWidget(mMainWidget->sieveEditorMainWidget());
-#endif
 
     mSpellCheckAction = new QAction(i18n("Check Spelling..."), this);
     connect(mSpellCheckAction, &QAction::triggered, mMainWidget->sieveEditorMainWidget(), &SieveEditorMainWidget::slotCheckSpelling);
