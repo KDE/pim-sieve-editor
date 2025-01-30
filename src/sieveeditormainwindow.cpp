382467f539c6ef05dfe0e2e58818096d3f1b8b7e
Laurent Montel <montel@kde.org>
2025-01-30 07:27:00 +0100

    Use SIEVEEDITOR_RELEASE_VERSION_DATE

--- src/sieveeditormainwindow.cpp
+++ src/sieveeditormainwindow.cpp
@@ -91,7 +91,7 @@ SieveEditorMainWindow::SieveEditorMainWindow(QWidget *parent)
     mainWidgetLayout->setContentsMargins({});
     mainWidgetLayout->setSpacing(0);
     if (PimCommon::NeedUpdateVersionUtils::checkVersion()) {
-        const auto status = PimCommon::NeedUpdateVersionUtils::obsoleteVersionStatus(KAboutData::applicationData().version(), QDate::currentDate());
+        const auto status = PimCommon::NeedUpdateVersionUtils::obsoleteVersionStatus(QLatin1String(SIEVEEDITOR_RELEASE_VERSION_DATE), QDate::currentDate());
         if (status != PimCommon::NeedUpdateVersionUtils::ObsoleteVersion::NotObsoleteYet) {
             auto needUpdateVersionWidget = new PimCommon::NeedUpdateVersionWidget(this);
             mainWidgetLayout->addWidget(needUpdateVersionWidget);
