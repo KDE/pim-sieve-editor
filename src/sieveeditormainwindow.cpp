333b60f9a74a191ee3b88f9e3ecd1eaf6d883927
Laurent Montel <montel@kde.org>
2025-11-08 22:14:15 +0100

    Fix stable url

--- src/sieveeditormainwindow.cpp
+++ src/sieveeditormainwindow.cpp
@@ -408,7 +408,7 @@ void SieveEditorMainWindow::setupActions()
 
 #if defined(Q_OS_WIN) || defined(Q_OS_MACOS)
 #if SIEVEEDITOR_STABLE_VERSION
-    const QString url = QStringLiteral("https://cdn.kde.org/ci-builds/pim/pim-sieve-editor/25.08/windows/");
+    const QString url = QStringLiteral("https://cdn.kde.org/ci-builds/pim/pim-sieve-editor/25.12/windows/");
 #else
     const QString url = QStringLiteral("https://cdn.kde.org/ci-builds/pim/pim-sieve-editor/master/windows/");
 #endif
