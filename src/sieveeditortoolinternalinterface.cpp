89e69ad194bd0e0c77290c10264f721768371dbb
Laurent Montel <montel@kde.org>
2026-03-27 13:20:57 +0100

    Prepare sieveeditortoolinternalinterface

---
+++ src/sieveeditortoolinternalinterface.cpp
@@ -0,0 +1,18 @@
+/*
+   SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "sieveeditortoolinternalinterface.h"
+
+SieveEditorToolInternalInterface::SieveEditorToolInternalInterface(QObject *parent)
+    : TextAutoGenerateText::TextAutoGenerateTextToolInternalInterface{parent}
+{
+}
+
+SieveEditorToolInternalInterface::~SieveEditorToolInternalInterface() = default;
+
+TextAutoGenerateText::TextAutoGenerateTextToolInternalJob *SieveEditorToolInternalInterface::callTool(const QByteArray &toolName)
+{
+}
---
+++ src/sieveeditortoolinternalinterface.h
@@ -0,0 +1,18 @@
+/*
+   SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+#include <TextAutoGenerateText/TextAutoGenerateTextToolInternalInterface>
+
+class SieveEditorToolInternalInterface : public TextAutoGenerateText::TextAutoGenerateTextToolInternalInterface
+{
+    Q_OBJECT
+public:
+    explicit SieveEditorToolInternalInterface(QObject *parent = nullptr);
+    ~SieveEditorToolInternalInterface() override;
+
+    [[nodiscard]] TextAutoGenerateText::TextAutoGenerateTextToolInternalJob *callTool(const QByteArray &toolName) override;
+};
