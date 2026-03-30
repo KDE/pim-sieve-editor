ce95fdface62f1fdb90e5160d7fc293276393058
Laurent Montel <montel@kde.org>
2026-03-30 20:09:47 +0200

    Prepare to use addnewscripttoolinternaljob

--- src/internaltoolsjob/addnewscripttoolinternaljob.cpp
+++ src/internaltoolsjob/addnewscripttoolinternaljob.cpp
@@ -5,6 +5,8 @@
 */
 
 #include "addnewscripttoolinternaljob.h"
+#include "sieveeditor_debug.h"
+#include "sieveeditormainwidget.h"
 using namespace Qt::Literals::StringLiterals;
 AddNewScriptToolInternalJob::AddNewScriptToolInternalJob(SieveEditorMainWidget *sieveEditorMainWidget, QObject *parent)
     : TextAutoGenerateText::TextAutoGenerateTextToolInternalJob{parent}
@@ -27,19 +29,21 @@ QByteArray AddNewScriptToolInternalJob::toolId()
 void AddNewScriptToolInternalJob::start()
 {
     if (mSieveEditorMainWidget) {
-        const QStringList lst = requiredArguments();
-        for (const auto &arg : lst) {
-            for (const auto &resultTool : std::as_const(mToolArguments)) {
-                if (resultTool.keyTool == arg) {
-                    // TODO
-                }
+        for (const auto &resultTool : std::as_const(mToolArguments)) {
+            if (resultTool.keyTool == "title"_L1) {
+                mSieveEditorMainWidget->createNewScript(); // TODO add title
+                // TODO
+            } else {
+                qCWarning(SIEVEEDITOR_LOG) << "Invalid json tools result:" << resultTool.keyTool;
             }
         }
+        deleteLater();
         // TODO
+        Q_EMIT finished({});
     } else {
+        qCWarning(SIEVEEDITOR_LOG) << "mSieveEditorMainWidget is not settings. It's a bug";
         deleteLater();
     }
-    // TODO
 }
 
 #include "moc_addnewscripttoolinternaljob.cpp"
