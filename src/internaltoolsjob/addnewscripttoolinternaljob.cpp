f64c1a21516b87037690c494caf407187b9cd872
Laurent Montel <montel@kde.org>
2026-03-31 08:35:37 +0200

    Implement job

--- src/internaltoolsjob/addnewscripttoolinternaljob.cpp
+++ src/internaltoolsjob/addnewscripttoolinternaljob.cpp
@@ -7,6 +7,7 @@
 #include "addnewscripttoolinternaljob.h"
 #include "sieveeditor_debug.h"
 #include "sieveeditormainwidget.h"
+#include <KLocalizedString>
 using namespace Qt::Literals::StringLiterals;
 AddNewScriptToolInternalJob::AddNewScriptToolInternalJob(SieveEditorMainWidget *sieveEditorMainWidget, QObject *parent)
     : TextAutoGenerateText::TextAutoGenerateTextToolInternalJob{parent}
@@ -29,21 +30,27 @@ QByteArray AddNewScriptToolInternalJob::toolId()
 void AddNewScriptToolInternalJob::start()
 {
     if (mSieveEditorMainWidget) {
+        QString title;
         for (const auto &resultTool : std::as_const(mToolArguments)) {
             if (resultTool.keyTool == "title"_L1) {
+                title = resultTool.value;
                 mSieveEditorMainWidget->createNewScript(); // TODO add title
-                // TODO
             } else {
                 qCWarning(SIEVEEDITOR_LOG) << "Invalid json tools result:" << resultTool.keyTool;
             }
         }
-        deleteLater();
-        // TODO
-        Q_EMIT finished({});
+        const TextAutoGenerateText::TextAutoGenerateTextToolInternalJob::TextToolPluginInfo info{
+            .content = i18n("Create New Sieve Script named %1", title),
+            .messageUuid = mMessageUuid,
+            .chatId = mChatId,
+            .toolIdentifier = mToolIdentifier,
+            .attachementInfoList = {},
+        };
+        Q_EMIT finished(info);
     } else {
         qCWarning(SIEVEEDITOR_LOG) << "mSieveEditorMainWidget is not settings. It's a bug";
-        deleteLater();
     }
+    deleteLater();
 }
 
 #include "moc_addnewscripttoolinternaljob.cpp"
