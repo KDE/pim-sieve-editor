2fb15194a8c8f46d1762d2f8aa14a5bbed19e8e0
Laurent Montel <montel@kde.org>
2026-05-20 07:31:38 +0200

    Add sieveeditorcommandlineparser

---
+++ src/sieveeditorcommandlineparser.cpp
@@ -0,0 +1,40 @@
+﻿/*
+ * SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#include "sieveeditorcommandlineparser.h"
+#include <KLocalizedString>
+#include <QCommandLineParser>
+
+using namespace Qt::Literals::StringLiterals;
+SieveEditorCommandLineParser::SieveEditorCommandLineParser(QCommandLineParser *parser)
+{
+    initializeCommandLine(parser);
+}
+
+SieveEditorCommandLineParser::~SieveEditorCommandLineParser() = default;
+
+void SieveEditorCommandLineParser::initializeCommandLine(QCommandLineParser *parser)
+{
+#if WITH_KUSERFEEDBACK
+    parser->addOption(QCommandLineOption(optionParserFromEnum(OptionParser::FeedBack), i18nc("@info:shell", "Lists the available options for user feedback")));
+#endif
+    QCommandLineOption selfTestOption(optionParserFromEnum(OptionParser::SelfTest), QStringLiteral("internal, for automated testing"));
+    selfTestOption.setFlags(QCommandLineOption::HiddenFromHelp);
+    parser->addOption(selfTestOption);
+}
+
+QString SieveEditorCommandLineParser::optionParserFromEnum(OptionParser e)
+{
+    switch (e) {
+#if WITH_KUSERFEEDBACK
+    case OptionParser::FeedBack:
+        return u"feedback"_s;
+#endif
+    case OptionParser::SelfTest:
+        return u"self-test"_s;
+    }
+    return {};
+}
---
+++ src/sieveeditorcommandlineparser.h
@@ -0,0 +1,27 @@
+﻿/*
+ * SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+#pragma once
+#include "config-pim-sieve-editor.h"
+#include "libsieveeditor_export.h"
+#include <QString>
+class QCommandLineParser;
+class LIBSIEVEEDITOR_EXPORT SieveEditorCommandLineParser
+{
+public:
+    enum class OptionParser : uint8_t {
+#if WITH_KUSERFEEDBACK
+        FeedBack,
+#endif
+        SelfTest,
+    };
+    explicit SieveEditorCommandLineParser(QCommandLineParser *parser);
+    ~SieveEditorCommandLineParser();
+
+    [[nodiscard]] static QString optionParserFromEnum(OptionParser e);
+
+private:
+    LIBSIEVEEDITOR_NO_EXPORT void initializeCommandLine(QCommandLineParser *parser);
+};
